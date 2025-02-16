c0f85fa5b0ee64727db60cac3fd548f4c6f3ae0d
Miguel Galán Lerate <miggaller@alum.us.es>
2025-02-16 01:55:55 +0100

    Feat: Application initialation

---
+++ src/test/java/secteam12/pai1/Pai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package secteam12.pai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pai1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
