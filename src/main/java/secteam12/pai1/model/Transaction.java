41269f14d67d79a85488ccb7820c62f3b1d04217
Miguel Galán Lerate <miggaller@alum.us.es>
2025-02-17 10:41:09 +0100

    Feat: Database creation

---
+++ src/main/java/secteam12/pai1/model/Transaction.java
@@ -0,0 +1,28 @@
+package secteam12.pai1.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String sourceAccount;
+
+    @Column(nullable = false)
+    private String destinationAccount;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+}
