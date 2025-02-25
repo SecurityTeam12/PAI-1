ecb1d968b3e88ecdf2e862abf90830920eae306e
Miguel Galán Lerate <miggaller@alum.us.es>
2025-02-25 12:56:24 +0100

    Feat: Add secure default users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (id, username,hash,salt) VALUES (1, 'user1', 'password1','testsalt1');
-INSERT INTO users (id, username, hash,salt) VALUES (2, 'user2', 'password2','testsalt2');
+INSERT INTO users (id, username,hash,salt) VALUES (1, 'user1', '$argon2id$v=19$m=65536,t=10,p=1$shtRnqQbb+Cio0jZTUUcEg$gP7Z7JZIKeOJQgBs1zaHspJpGcErWwze575TD79uUGE','D18qSuQCacGa2wqFxoqWwg==');
+INSERT INTO users (id, username, hash,salt) VALUES (2, 'user2', '$argon2id$v=19$m=65536,t=10,p=1$FP0jghC/yH+tcnMDzX9lWw$ny/HHspfKfmJ4xbWsDLOJtMXFKZfzekP1tH2WgN8pi4','2kJkjkpyoCe+wGZeRc2ckQ==');
 
